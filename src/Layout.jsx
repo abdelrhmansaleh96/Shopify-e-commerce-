820ac41b27450d9c0eff74485455de9011a3d7b6
abdelrhman saleh <81262213+abdelrhmansaleh96@users.noreply.github.com>
2022-09-14 16:37:43 +0200

    fixing scrolling to top after page change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../src/components/Navbar/Navbar";
 import Discount from "../src/components/Discount/Discount";
 import Footer from "../src/components/Footer/Footer";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return (
     <>
       <Navbar />
