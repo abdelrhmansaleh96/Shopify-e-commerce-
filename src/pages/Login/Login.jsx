b9bbc2672457c7a6f02cc81499264acca5169d8e
abdelrhman saleh <81262213+abdelrhmansaleh96@users.noreply.github.com>
2022-09-21 13:35:45 +0200

    responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Login.scss";
 import Tabs from "@mui/material/Tabs";
-import { Button, Tab, TextField } from "@mui/material";
+import { Button, Tab, TextField, Container } from "@mui/material";
 import MainBreadcrumb from "../../components/MainBreadcrumb/MainBreadcrumb";
 import { useLocation } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
@@ -48,122 +48,182 @@ const Login = () => {
     <div className="Login">
       <MainBreadcrumb location={location} />
       <div className="container">
-        <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Login" />
-          <Tab label="Register" />
-        </Tabs>
-        {value === 0 ? (
-          <div className="signIn">
-            <TextField
-              id="outlined-basic"
-              label="Email"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-              variant="outlined"
-              sx={{
-                width: "650px",
-                marginBottom: "35px",
-              }}
-            />
-            <FormControl sx={{ m: 1, width: "650px" }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={values.showPassword ? "text" : "password"}
-                value={values.password}
-                onChange={handleChangeForPass("password")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-            <Button
-              variant="outlined"
-              sx={{ width: "650px", margin: "35px 0" }}
-              // onClick={addUser(mail)}
-              onClick={() => {
-                addUser(mail);
-              }}
-            >
-              SignIn
-            </Button>
-            <Button variant="contained">Forget Password</Button>
-          </div>
-        ) : (
-          <div className="signIn">
-            <div className="name">
+        <Container
+          maxWidth="lg"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            overflow: "hidden",
+            gap: "20px",
+          }}
+        >
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Login" />
+            <Tab label="Register" />
+          </Tabs>
+          {value === 0 ? (
+            <div className="signIn">
               <TextField
                 id="outlined-basic"
-                label="First Name"
+                label="Email"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
                 variant="outlined"
                 sx={{
-                  width: "300px",
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
                   marginBottom: "35px",
                 }}
               />
-              <TextField
-                id="outlined-basic"
-                label="Last Name"
+              <FormControl
+                sx={{
+                  m: 1,
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
+                }}
+                variant="outlined"
+              >
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? "text" : "password"}
+                  value={values.password}
+                  onChange={handleChangeForPass("password")}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button
                 variant="outlined"
                 sx={{
-                  width: "300px",
-                  marginBottom: "35px",
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
+                  margin: "35px 0",
                 }}
-              />
+                // onClick={addUser(mail)}
+                onClick={() => {
+                  addUser(mail);
+                }}
+              >
+                SignIn
+              </Button>
+              <Button variant="contained">Forget Password</Button>
             </div>
-            <TextField
-              id="outlined-basic"
-              label="Email"
-              variant="outlined"
-              sx={{
-                width: "650px",
-                marginBottom: "35px",
-              }}
-            />
-            <FormControl sx={{ m: 1, width: "650px" }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={values.showPassword ? "text" : "password"}
-                value={values.password}
-                onChange={handleChangeForPass("password")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
+          ) : (
+            <div className="signIn">
+              <div className="name">
+                <TextField
+                  id="outlined-basic"
+                  label="First Name"
+                  variant="outlined"
+                  sx={{
+                    width: {
+                      xs: "120px",
+                      md: "300px",
+                    },
+                    marginBottom: "35px",
+                  }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Last Name"
+                  variant="outlined"
+                  sx={{
+                    width: {
+                      xs: "120px",
+                      md: "300px",
+                    },
+                    marginBottom: "35px",
+                  }}
+                />
+              </div>
+              <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                sx={{
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
+                  marginBottom: "35px",
+                }}
               />
-            </FormControl>
-            <Button
-              variant="contained"
-              sx={{ width: "650px", margin: "35px 0" }}
-            >
-              Create Account
-            </Button>
-          </div>
-        )}
+              <FormControl
+                sx={{
+                  m: 1,
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
+                }}
+                variant="outlined"
+              >
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? "text" : "password"}
+                  value={values.password}
+                  onChange={handleChangeForPass("password")}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? (
+                          <VisibilityOff />
+                        ) : (
+                          <Visibility />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button
+                variant="contained"
+                sx={{
+                  width: {
+                    xs: "250px",
+                    md: "650px",
+                  },
+                  margin: "35px 0",
+                }}
+              >
+                Create Account
+              </Button>
+            </div>
+          )}
+        </Container>
       </div>
     </div>
   );
